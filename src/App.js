8942bcf196d857fc2f31d5de57458914f01b3874
tywi6665 <tyletcole@gmail.com>
2020-05-29 13:45:41 -0600

    created currentPage state and passed it in as prop to Pagination component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Container from "./Components/Container";
 import Table from "./Components/Table/Table";
+import Pagination from "./Components/Pagination";
 import API from "./utils/API";
 import "./App.scss";
 
@@ -9,6 +10,10 @@ function App() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [restaurants, setRestaurants] = useState([]);
+  const [currentPage, setCurrentPage] = useState({
+    currentPage: 1,
+    resultsPerPage: 10
+  });
 
   useEffect(() => {
     API.fetchRestaurants().then(res => {
@@ -20,6 +25,10 @@ function App() {
 
   return (
     <Container>
+      <Pagination
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
       <Table
         data={restaurants}
       />
--- src/Components/Pagination.js
+++ src/Components/Pagination.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-const Pagination = () => {
+const Pagination = ({ currentPage, setCurrentPage }) => {
     return (
         <div>
             <ul>
