13006c6bcda2ce9f17e1161fd777e7a72f60d97f
tywi6665 <tyletcole@gmail.com>
2020-05-29 14:19:50 -0600

    created Filter.js component

---
+++ src/Components/Search/Filter.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Filter = () => {
+    return (
+        <div>
+            <div>
+                <label>Filter by Genre: </label>
+                <select>
+                    <option value="-All-">-All-</option>
+                </select>
+                <label>Filter by State: </label>
+                <select>
+                    <option value="-All-">-All-</option>
+                </select>
+            </div>
+        </div>
+    );
+}
+
+export default Filter;
