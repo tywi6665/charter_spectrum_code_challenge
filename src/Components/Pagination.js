1dbf650d9fba1807bf833c0d39c625a08089b47f
tywi6665 <tyletcole@gmail.com>
2020-05-29 13:42:10 -0600

    created Pagination Component

---
+++ src/Components/Pagination.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+
+const Pagination = () => {
+    return (
+        <div>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Pagination;
