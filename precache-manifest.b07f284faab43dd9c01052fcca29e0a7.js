7a7fed6b79a5ac8bcdf8f9befb0dbdb7270a76a7
tywi6665 <tyletcole@gmail.com>
2020-05-29 22:14:33 -0600

    Updates

---
+++ precache-manifest.b07f284faab43dd9c01052fcca29e0a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4a67debfe170316b1607f461fe6f934",
+    "url": "/charter_spectrum_code_challenge/index.html"
+  },
+  {
+    "revision": "80b6d346d7d5a0663991",
+    "url": "/charter_spectrum_code_challenge/static/css/main.b6261e14.chunk.css"
+  },
+  {
+    "revision": "73a05144d7dc687a88f8",
+    "url": "/charter_spectrum_code_challenge/static/js/2.ae60bf33.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/charter_spectrum_code_challenge/static/js/2.ae60bf33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80b6d346d7d5a0663991",
+    "url": "/charter_spectrum_code_challenge/static/js/main.f72b02a5.chunk.js"
+  },
+  {
+    "revision": "15669f828c83b0d501f4",
+    "url": "/charter_spectrum_code_challenge/static/js/runtime-main.3b912e5f.js"
+  }
+]);
